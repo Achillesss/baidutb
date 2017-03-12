1b7a09fceea5a96fa589424309739fb80dfe24d3
Achillesss <achillesss@126.com>
2017-03-13 00:42:46 +0800

    fix gorutine bug: current write maps

--- client/resquet.go
+++ client/resquet.go
@@ -12,39 +12,31 @@ import (
 	req "github.com/parnurzeal/gorequest"
 )
 
-func (a *agent) doRequest() (resp []byte) {
-	if _, resp, _ = a.req.EndBytes(); resp == nil {
-		a.err = fmt.Errorf("nil reponse body")
+func get(url, bduss, kw string) (res []byte) {
+	r := req.New().CustomMethod("GET", url).Set("Cookie", fmt.Sprintf("BDUSS=%s", bduss)).Param("fname", kw)
+	_, res, _ = r.EndBytes()
+	if debug {
+		s, _ := r.AsCurlCommand()
+		log.Printfln("[CURL] %s", s)
+	}
+
+	return
+}
+
+func parseListResp(resp []byte) (kwList []string) {
+	if resp != nil {
+		reg := regexp.MustCompile(`\d+\.[<][a]\s\w+[=][[:ascii:]]+[>](\S+|[[:word:]+])[<]\/[a][>]`)
+		g := reg.FindAllStringSubmatch(string(resp), -1)
+		for i := range g {
+			kwList = append(kwList, g[i][1])
+		}
+		// log.Printfln("User %s tieba list:\t%s", a.params["bduss"], kwList)
 	}
 	return
 }
 
-func (a *agent) get(url, kw string) *agent {
-	if a.err == nil {
-		a.req = req.New().CustomMethod("GET", url).Set("Cookie", fmt.Sprintf("BDUSS=%s", a.params["bduss"])).Param("fname", kw)
-		if debug {
-			s, _ := a.req.AsCurlCommand()
-			log.Printfln("[CURL] %s", s)
-		}
-	}
-	return a
-}
-
-func (a *agent) parseListResp(resp []byte) *agent {
-	if resp != nil {
-		reg := regexp.MustCompile(`\d+\.[<][a]\s\w+[=][[:ascii:]]+[>](\S+|[[:word:]+])[<]\/[a][>]`)
-		g := reg.FindAllStringSubmatch(string(resp), -1)
-		a.kwList = make(map[string]string)
-		for i := range g {
-			a.kwList[g[i][1]] = time.Now().Format(time.RFC3339)
-		}
-		log.Printfln("User %s tieba list:\t%s", a.params["bduss"], a.kwList)
-	}
-	return a
-}
-
-func (a *agent) getList() []byte {
-	return a.get(a.listURL, "").doRequest()
+func getList(bduss string, conf *config.C) []byte {
+	return get(conf.ListURL, bduss, "")
 }
 
 func (a *agent) parseTbsResp(resp []byte) *agent {
@@ -58,7 +50,7 @@ func (a *agent) parseTbsResp(resp []byte) *agent {
 }
 
 func (a *agent) getTbs() []byte {
-	return a.get(a.tbsURL, "").doRequest()
+	return get(a.tbsURL, a.params["bduss"], "")
 }
 
 func (a *agent) parseFidResp(resp []byte) *agent {
@@ -75,7 +67,7 @@ func (a *agent) parseFidResp(resp []byte) *agent {
 	return a
 }
 func (a *agent) getFid(kw string) []byte {
-	return a.get(a.fidURL, kw).doRequest()
+	return get(a.fidURL, a.params["bduss"], kw)
 }
 
 func (a *agent) signUp(kw string) time.Time {
@@ -100,14 +92,6 @@ func (a *agent) signUp(kw string) time.Time {
 	return t
 }
 
-func (a *agent) startToSign(kw string) {
-	if a.err == nil {
-		if a.parseFidResp(a.getFid(kw)).ok() {
-			signingTimeSlice = append(signingTimeSlice, a.sign(kw).signUp(kw))
-		}
-	}
-}
-
 func transBdussChan(bdussChan chan<- string, bdussList []string) {
 	for _, b := range bdussList {
 		bdussChan <- b
@@ -115,30 +99,35 @@ func transBdussChan(bdussChan chan<- string, bdussList []string) {
 	close(bdussChan)
 }
 
-func transKw(kwChan chan<- string, kwList map[string]string) {
-	for k := range kwList {
+func transKw(kwChan chan<- string, kwList []string) {
+	for _, k := range kwList {
 		kwChan <- k
 	}
 	close(kwChan)
 }
 
-func (a *agent) signByKw(kwChan <-chan string) {
+func startToSign(bduss, kw string, conf *config.C) {
+	a := new(agent)
+	a.params = make(map[string]string)
+	if a.configurate(conf).setBduss(bduss).parseTbsResp(a.getTbs()).parseFidResp(a.getFid(kw)).ok() {
+		signingTimeSlice = append(signingTimeSlice, a.sign(kw).signUp(kw))
+	}
+	a.log()
+}
+
+func signByKw(bduss string, kwChan <-chan string, conf *config.C) {
 	for k := range kwChan {
 		go func(kw string) {
-			a.startToSign(kw)
+			startToSign(bduss, kw, conf)
 		}(k)
 	}
 }
 
 func signOnePerson(bduss string, conf *config.C) {
-	a := new(agent)
-	a.configurate(conf)
-	a.params = make(map[string]string)
-	a.setBduss(bduss).parseListResp(a.getList()).parseTbsResp(a.getTbs())
+	kwList := parseListResp(getList(bduss, conf))
 	kwChan := make(chan string)
-	go transKw(kwChan, a.kwList)
-	a.signByKw(kwChan)
-	a.log()
+	go transKw(kwChan, kwList)
+	signByKw(bduss, kwChan, conf)
 }
 
 func signByBDUSS(bdussChan <-chan string, conf *config.C) (res map[string]bool) {
